37000a8dbc264e514979ce52121b4f0bcf87d267
Alex Nagy <therebeloper@gmail.com>
2022-05-04 20:53:14 +0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# CustomFrameworkDemo
+
+A description of this package.
---
+++ Sources/CustomFrameworkDemo/CustomFrameworkDemo.swift
@@ -0,0 +1,6 @@
+public struct CustomFrameworkDemo {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}

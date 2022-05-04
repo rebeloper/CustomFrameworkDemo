5d37cef4bd965fccb25bc8def8cedd9a27311197
Alex Nagy <therebeloper@gmail.com>
2022-05-04 21:04:47 +0300

    first commit

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "CustomFrameworkDemo",
+    platforms: [
+      .macOS(.v12), .iOS(.v15)
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -18,11 +21,8 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
+        .binaryTarget(
             name: "CustomFrameworkDemo",
-            dependencies: []),
-        .testTarget(
-            name: "CustomFrameworkDemoTests",
-            dependencies: ["CustomFrameworkDemo"]),
+            path: "./Sources/CustomFrameworkDemo.xcframework")
     ]
 )
---
+++ Sources/CustomFrameworkDemo.xcframework/ios-arm64/CustomFrameworkDemo.framework/Headers/CustomFrameworkDemo.h
@@ -0,0 +1,18 @@
+//
+//  CustomFrameworkDemo.h
+//  CustomFrameworkDemo
+//
+//  Created by Alex Nagy on 04.05.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CustomFrameworkDemo.
+FOUNDATION_EXPORT double CustomFrameworkDemoVersionNumber;
+
+//! Project version string for CustomFrameworkDemo.
+FOUNDATION_EXPORT const unsigned char CustomFrameworkDemoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CustomFrameworkDemo/PublicHeader.h>
+
+
